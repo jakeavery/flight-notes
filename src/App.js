d90ea2f54d9ccf3883d64798da51b67969723fc7
Jake <jakeavery@jakeavery.me>
2018-07-17 17:51:08 -0700

    It's working! It's working!

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-
 // Import bootstrap components
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+// Import other components
+import MissionInfo from './components/MissionInfo';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>
+      <Grid>
+        <MissionInfo />
+      </Grid>
     );
   }
-}
+};
 
 export default App;
---
+++ src/components/ComponentTemplate.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+// Import bootstrap components, ex:
+//import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+// Import other componenst, ex:
+//import FlightSection from './components/FlightSection'
+
+import './App.css';
+
+class ComponentTemplate extends Component {
+  // Pass props if required
+  constructor () {
+    // Pass props if requirted
+    super();
+    this.state = {
+      // Initialize things that change here
+    }
+  }
+
+    componentDidMount() {
+      // Fetch data then setState here
+    }
+
+    render() {
+      return (
+        // Build component here
+      )
+    }
+};
+
+export default ComponentTemplate;
