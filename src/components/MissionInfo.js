5b9b2c4d2a36ed2afc44830956a608a0276548a3
Jake <jakeavery@jakeavery.me>
2018-07-17 21:05:26 -0700

    Rough Mission Info layout

--- src/components/MissionInfo.js
+++ src/components/MissionInfo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // Import bootstrap components, ex:
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 // Import other componenst, ex:
 // import FlightSection from './components/FlightSection'
 
@@ -10,12 +10,25 @@ import '../App.css';
 class MissionInfo extends Component {
   render() {
     return (
-    // Build component here
-      <Row className="show-grid">
-        <h1>
-Mission Info
-        </h1>
-      </Row>
+      // Build component here
+      <div>
+        <Row className="show-grid">
+          <h1>Mission Info</h1>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={2}>10/08/1991</Col>
+          <Col xs={2}>Flight 1</Col>
+          <Col xs={4}>Location</Col>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={4}>Pilot-in-Command</Col>
+          <Col xs={4}>Payload Operator</Col>
+          <Col xs={4}>Visual Observer</Col>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={12}>Mission Description</Col>
+        </Row>
+      </div>
     );
   }
 }
